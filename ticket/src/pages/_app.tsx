3d1ef742b87d5093af938b5c0e947be8c632ef6b
Nelson Neto <nelsonsilvaneto1988@gmail.com>
2023-11-01 18:08:32 -0400

    auth funfando

--- ticket/src/pages/_app.tsx
+++ ticket/src/pages/_app.tsx
@@ -2,10 +2,13 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
-    <SessionProvider>
-      <Component {...pageProps} />;
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
     </SessionProvider>
   );
 }
--- ticket/src/pages/index.tsx
+++ ticket/src/pages/index.tsx
@@ -1,66 +1,101 @@
 import { GetServerSideProps } from "next";
 import { getSession, signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+import { FormEventHandler, useState } from "react";
 
 export default function Home() {
+  const { push } = useRouter();
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const InputValue = (e) =>
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    const result = await signIn("credentials", {
+      ...data,
+      redirect: false,
+      callbackUrl: "/",
+    });
+
+    if (result?.url) {
+      return push(result?.url);
+    }
+  };
   return (
-    <section className="flex items-center justify-center h-[110vh]">
-      <div className="flex flex-col items-center">
-        <h2 className="m-[1rem] font-extrabold text-[25px]">Faça o login</h2>
+    <section className="flex items-center justify-center h-[100vh]">
+      <div className="flex flex-col items-center lg:w-96 sm:w-auto">
+        <div className="p-4 text-white font-extrabold text-[25px]">
+          <h2>Ticket Contec</h2>
+        </div>
         <form
           action=""
           method="post"
+          autoComplete="off"
           className="flex flex-col gap-2 w-[100%] max-w-[100%]"
+          onSubmit={handleSubmit}
         >
           <div className="flex flex-col gap-2">
             <label htmlFor="email">E-mail</label>
             <input
               type="text"
               name="email"
-              className="p-[10px] bg-slate-900 rounded-md"
+              className="p-[10px] bg-slate-900 outline-0 rounded"
+              onChange={InputValue}
             />
           </div>
           <div className="flex flex-col gap-2">
             <label htmlFor="password">Password</label>
             <input
-              type="text"
+              type="password"
               name="password"
-              className="p-[10px] bg-slate-900 rounded-md"
+              className="p-[10px] bg-slate-900 outline-0 rounded"
+              onChange={InputValue}
             />
           </div>
           <div>
             <p>
-              Não possui conta? <span>Cadastre-se</span>
+              Não tem conta? <span>Cadastre-se!</span>
             </p>
           </div>
           <button
             type="submit"
-            className="bg-slate-50 hover:bg-slate-500 p-[10px] text-blue-700 font-medium rounded-md"
+            className="bg-blue-800 houver: p-[10px] text-gray-50 font-medium rounded"
           >
-            Entrar
+            Fazer Login
           </button>
+
           <fieldset className="border-t border-slate-50 mt-[10px]">
             <legend className="mx-auto px-4 text-white text-1xl italic">
               ou
             </legend>
-            <div className="text-white pt-4 text-center">Acesse com o seu:</div>
+            <div className="text-white pt-4">Acesse com o seu:</div>
           </fieldset>
           <button
             onClick={() => signIn("github")}
             type="button"
-            className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex justify-center items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2"
+            className="flex justify-center text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2"
           >
             <svg
-              className="w-4 h-4 mr-2"
+              className="w-4 h-4 mr-2 -ml-1"
               aria-hidden="true"
+              focusable="false"
+              data-prefix="fab"
+              data-icon="github"
+              role="img"
               xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 20 20"
+              viewBox="0 0 496 512"
             >
               <path
-                fill-rule="evenodd"
-                d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
-                clip-rule="evenodd"
-              />
+                fill="currentColor"
+                d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
+              ></path>
             </svg>
             Sign in with Github
           </button>
