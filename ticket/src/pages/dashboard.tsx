c8c020a29a8ee6815e932a35bb78bdc9b940c97c
Nelson Neto <nelsonsilvaneto1988@gmail.com>
2023-11-03 18:05:30 -0400

    dashoboard começandiii

--- ticket/src/pages/dashboard.tsx
+++ ticket/src/pages/dashboard.tsx
@@ -7,7 +7,19 @@ export default function Dashboard() {
     <section className="flex items-center justify-center h-[100vh]">
       <div className="flex flex-col items-center">
         <h2 className="m-[1rem] font-extrabold text-[30px]">Dashboard</h2>
-        {session?.user?.name}
+        <div className="flex flex-wrap">
+          <div className="">
+            <img
+              className="rounded-full w-[80px] h-[80px]"
+              src="https://avatars.githubusercontent.com/u/86265413?v=4"
+              alt="user avatar"
+            />
+          </div>
+          <div className="flex flex-col ml-6 justify-center">
+            <h2> {session?.user?.name}</h2>
+            <p> {session.user.email}</p>
+          </div>
+        </div>
         <button onClick={() => signOut()}>Sair</button>
       </div>
     </section>
