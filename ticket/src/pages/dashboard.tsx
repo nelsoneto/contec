211d23a888e090c6128c6203931ac2f9c46f4b56
Nelson Neto <nelsonsilvaneto1988@gmail.com>
2023-11-07 17:56:28 -0400

    navbar teste

--- ticket/src/pages/dashboard.tsx
+++ ticket/src/pages/dashboard.tsx
@@ -7,25 +7,30 @@ export default function Dashboard() {
   console.log(session);
 
   return (
-    <section className="flex items-center justify-center h-[100vh]">
+    <section className="flex  items-center justify-center h-[100vh]">
       <div className="flex flex-col items-center">
-        <h2 className="m-[1rem] font-extrabold text-[30px]">Dashboard</h2>
-        <div className="flex flex-wrap">
+        <h2 className="m-[1rem] mb-12 font-extrabold text-[30px]">Dashboard</h2>
+        <div className="flex">
           <div className="">
-            {/* <Image
-              className="rounded-full w-[80px]"
-              src={session.user.image}
+            <Image
+              className=" rounded-full w-[70px]"
+              src={session?.user?.image}
               width="200"
               height="200"
               alt="user avatar"
-            /> */}
+            />
           </div>
-          <div className="flex flex-col ml-6 justify-center">
-            <h2> {session?.user?.name}</h2>
+          <div className="flex flex-col ml-6 justify-center  text-white">
+            <h2 className="font-semibold">{session?.user?.name}</h2>
             <p> {session.user.email}</p>
           </div>
         </div>
-        <button onClick={() => signOut()}>Sair</button>
+        <button
+          className="font-extrabold mt-5 bg-blue-700 hover:bg-blue-500 text-white w-[85px] h-[35px] rounded-lg"
+          onClick={() => signOut()}
+        >
+          Sair
+        </button>
       </div>
     </section>
   );
