6caeabcbb8707508c9255c0a92ba9edb9f3680e5
Nelson Neto <nelsonsilvaneto1988@gmail.com>
2024-10-19 11:33:57 -0400

    navbar bug

--- site/src/components/Navbar.tsx
+++ site/src/components/Navbar.tsx
@@ -40,7 +40,7 @@ export function Navbar() {
         </div>
         {/* linke items */}
         <ul
-          className={`absolute left-0 z-[-1] w-full pb-12 pl-5 transition-all duration-500 ease-linear md:static md:z-auto md:flex md:w-auto md:items-center md:pb-0 md:pl-0 ${open ? 'top-12' : 'top-[-490px]'}`}
+          className={`absolute left-0 z-[-1] w-full bg-slate-900 pb-12 pl-5 transition-all duration-500 ease-linear md:static md:z-auto md:flex md:w-auto md:items-center md:pb-0 md:pl-0 ${open ? 'top-12' : 'top-[-490px]'}`}
         >
           {Links.map((link) => (
             <li key={link.id} className="my-7 py-2 md:my-0 md:ml-8">
