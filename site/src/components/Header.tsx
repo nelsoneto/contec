d64438c21979ee24f79259da0785355eaf96c41f
Nelson Neto <nelsonsilvaneto1988@gmail.com>
2024-10-23 11:01:11 -0400

    widget concluído

--- site/src/components/Header.tsx
+++ site/src/components/Header.tsx
@@ -1,9 +1,7 @@
-import Link from 'next/link'
-
 export function Header() {
   return (
     // eslint-disable-next-line prettier/prettier
-    <header className="mt-9 h-auto w-auto bg-cover bg-stars bg-center px-4 py-32 md:bg-bannerImg xl:h-screen xl:px-60 xl:py-64">
+    <header className="mt-9 h-auto w-auto bg-cover bg-stars bg-center px-10 py-32  xl:h-screen xl:px-60 xl:py-48">
       <div className="w-full items-center justify-center gap-7 rounded-lg md:grid md:grid-cols-2">
         <div className="items-center justify-center gap-10 text-balance rounded-lg">
           <div className="w-auto rounded-md text-center font-alt text-4xl md:text-start md:text-5xl xl:py-28 xl:text-6xl">
@@ -13,21 +11,6 @@ export function Header() {
             </h1>
           </div>
         </div>
-        {/* <div className="rounded-lg bg-blue-500 px-3 py-4 shadow-md ring-8 ring-blue-200">
-          <img
-            alt="city"
-            src={heroImg}
-            className="h-auto w-auto rounded-xl object-cover"
-          />
-        </div> */}
-      </div>
-      <div className="flex w-full justify-end text-xs text-zinc-500">
-        <Link
-          rel="noreferrer"
-          href="https://www.freepik.com/free-photo/close-up-portrait-smiling-girl-entrepreneur-office-woman-glasses-looking-happy-confident-s_22531011.htm#from_view=detail_alsolike"
-        >
-          Image by benzoix on Freepik
-        </Link>
       </div>
     </header>
   )
